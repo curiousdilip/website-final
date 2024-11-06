4222c83d09c3d85aafcf5a3db7cbc3af580eb67e
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-11-06 23:20:44 +0530

    add some link

--- app/components/ProfileCard.jsx
+++ app/components/ProfileCard.jsx
@@ -1,44 +1,58 @@
-import React from 'react'
-import "./profile-card.scss"
-import Image from 'next/image'
+import React from "react";
+import "./profile-card.scss";
+import Image from "next/image";
 const ProfileCard = () => {
-    return (
-        <>
-            <div className="line"></div>
+  return (
+    <>
+      <div className="line"></div>
 
-            <div className="profile-box">
-                <div className="img-box">
-                    <Image
-                        src="/profilecardimg.jpeg"
-                        alt='Profile Card Image'
-                        height={100}
-                        width={390}
-                        className='img-fluid'
-                        priority
-                    />
-                </div>
-                <div className="details">
-                    <div className="name"><h2>Dilip Kumar </h2>
-                        <h3>Web Developer</h3></div>
-                    <div className="work">
-                        <i className='bi bi-briefcase'>World Health Organization</i>
-                    </div>
-                    <div className="location">
-                        <a href="https://www.google.com/maps/place/Dilip+Kumar+%7C+Frontend+Web+Developer/@28.569776,77.0265519,20.3z/data=!4m6!3m5!1s0x390d1b0024a3520b:0x7cca2b1632be5ac7!8m2!3d28.5695894!4d77.0267466!16s%2Fg%2F11r__7nsmj?entry=ttu" target='_blank'><i className='bi bi-geo'> New Delhi, India</i></a>
-                    </div>
-                    <div className="mail">
-                        <a href="mailto:curiousdilip@gmail.com"> <i className='bi bi-envelope'> curiousdilip@gmail.com</i></a>
-                    </div>
-                    <div className="social">
-                        <a href="https://github.com/curiousdilip" target='_blank'> <i className='bi bi-github'> curiousdilip</i></a>
-                        <a href="https://www.linkedin.com/in/curiousdilip/" target='_blank'><i className='bi bi-linkedin'> curiousdilip</i></a>
+      <div className="profile-box">
+        <div className="img-box">
+          <Image
+            src="/profilecardimg.jpeg"
+            alt="Profile Card Image"
+            height={100}
+            width={390}
+            className="img-fluid"
+            priority
+          />
+        </div>
+        <div className="details">
+          <div className="name">
+            <h2>Dilip Kumar </h2>
+            <h3>Web Developer</h3>
+          </div>
+          <div className="work">
+            <i className="bi bi-briefcase">World Health Organization</i>
+          </div>
+          <div className="location">
+            <a
+              href="https://www.google.com/maps/place/Dilip+Kumar+%7C+Frontend+Web+Developer/@28.569776,77.0265519,20.3z/data=!4m6!3m5!1s0x390d1b0024a3520b:0x7cca2b1632be5ac7!8m2!3d28.5695894!4d77.0267466!16s%2Fg%2F11r__7nsmj?entry=ttu"
+              target="_blank"
+            >
+              <i className="bi bi-geo"> New Delhi, India</i>
+            </a>
+          </div>
+          <div className="mail">
+            <a href="mailto:curiousdilip@gmail.com">
+              {" "}
+              <i className="bi bi-envelope"> curiousdilip@gmail.com</i>
+            </a>
+          </div>
+          <div className="social">
+            <a href="https://github.com/curiousdilip" target="_blank">
+              {" "}
+              <i className="bi bi-github"> curiousdilip</i>
+            </a>
+            <a href="https://www.linkedin.com/in/curiousdilip/" target="_blank">
+              <i className="bi bi-linkedin"> curiousdilip</i>
+            </a>
+          </div>
+        </div>
+      </div>
+      <div className="line-1"></div>
+    </>
+  );
+};
 
-                    </div>
-                </div>
-            </div>
-            <div className="line-1"></div>
-        </>
-    )
-}
-
-export default ProfileCard
+export default ProfileCard;
--- app/page.jsx
+++ app/page.jsx
@@ -1,13 +1,39 @@
+import Link from "next/link";
+
 export default function Home() {
-  return <>
-    <section id="hero">
-      <div className="container">
-        <div className="info">
-          <h1>My name is Dilip</h1>
-          <p>I'm Dilip Kumar Maurya, a web developer with over a year of experience. I'm currently working as a Web Content Executive at the World Health Organization.</p>
-          <p>I'm passionate about technology and computers, and I'm always eager to learn new things. I've previously done freelance work, and I'm always looking for new challenges</p>
+  return (
+    <>
+      <section id="hero">
+        <div className="container">
+          <div className="info">
+            <h1>My name is Dilip</h1>
+            <p>
+              I'm Dilip Kumar Maurya, a web developer with over a year of
+              experience. I'm currently working as a Web Content Executive at
+              the World Health Organization.
+            </p>
+            <p>
+              I'm passionate about technology and computers, and I'm always
+              eager to learn new things. I've previously done freelance work,
+              and I'm always looking for new challenges.
+            </p>
+            <ul>
+              <li>
+                <Link href="/">What I have Built?</Link>
+              </li>{" "}
+              <li>
+                <Link href="/">More about myself</Link>
+              </li>{" "}
+              <li>
+                <Link href="/">My Latest Resume</Link>
+              </li>{" "}
+              <li>
+                <Link href="/">Contact Me</Link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </div>
-    </section>
-  </>;
+      </section>
+    </>
+  );
 }

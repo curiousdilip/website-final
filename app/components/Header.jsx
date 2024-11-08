9474c4021a897c70c86f5359c60d91f93d37cc9d
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-11-08 22:33:29 +0530

    work

--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -1,24 +1,43 @@
 import Link from "next/link";
-import "./header.scss"
+import "./header.scss";
 export default function Header() {
-  return <>
-    <header>
-    <nav>
-        <ul className='nav-bar'>
-            <li className='logo'><Link href='/'><img src='/img/logo-white.svg'/></Link></li>
-            <input type='checkbox' id='check' />
+  return (
+    <>
+      <header>
+        <nav>
+          <ul className="nav-bar">
+            <li className="logo">
+              <Link href="/">
+                <img src="/img/logo-white.svg" />
+              </Link>
+            </li>
+            <input type="checkbox" id="check" />
             <span className="menu">
-                <li><Link href="/">Home</Link></li>
-                <li><Link href="/about">About</Link></li>
-                <li><Link href="/work">Work</Link></li>
-                <li><Link href="/uses">Uses</Link></li>
-                <li><Link href="/contact">Contact</Link></li>
-                <label for="check" className="close-menu"><i className="bi bi-x-lg"></i></label>
+              <li>
+                <Link href="/">Home</Link>
+              </li>
+              <li>
+                <Link href="/about">About</Link>
+              </li>
+              <li>
+                <Link href="/work">Work</Link>
+              </li>
+              <li>
+                <Link href="/uses">Uses</Link>
+              </li>
+              <li>
+                <Link href="/contact">Contact</Link>
+              </li>
+              <label htmlFor="check" className="close-menu">
+                <i className="bi bi-x-lg"></i>
+              </label>
             </span>
-            <label for="check" className="open-menu"><i className="bi bi-list"></i></label>
-        </ul>
-    </nav>
-    </header>
-      
-   </>;
+            <label htmlFor="check" className="open-menu">
+              <i className="bi bi-list"></i>
+            </label>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
--- app/work/page.jsx
+++ app/work/page.jsx
@@ -1,44 +1,44 @@
 import Image from "next/image";
-import { projects } from '../data';
-import "./work.scss"
+import { projects } from "../data";
+import "./work.scss";
 import Link from "next/link";
 export default function Work() {
-    return <section id="work">
-        <div className="container">
-            <h1 className="work-heading">Projects</h1>
+  return (
+    <section id="work">
+      <div className="container">
+        <h1 className="work-heading">Projects</h1>
 
-            <div className="projects">
-                {projects.map((project, index) => (
-                    <div className="project" key={index}>
-                        <Image
-                            src={project.imgSrc}
-                            alt={project.title}
-                            width={640}
-                            height={320}
-                            priority="true"
-                            className='img-fluid'
-                        />
-                        <div className="details">
-                            <h3 className="p-title">{project.title}</h3>
-                            <p className="description">{project.description}</p>
+        <div className="projects">
+          {projects.map((project, index) => (
+            <div className="project" key={index}>
+              <Image
+                src={project.imgSrc}
+                alt={project.title}
+                width={300}
+                height={200}
+                priority="true"
+                layout="intrinsic"
+              />
+              <div className="details">
+                <h3 className="p-title">{project.title}</h3>
+                <p className="description">{project.description}</p>
 
-                            {project.tech ? (
-                                <ul className="tech-stack">
-                                    {project.tech.map((technology, techIndex) => (
-                                        <li key={techIndex}>{technology}</li>
-                                    ))}
-                                </ul>
-                            ) : null}
+                {project.tech ? (
+                  <ul className="tech-stack">
+                    {project.tech.map((technology, techIndex) => (
+                      <li key={techIndex}>{technology}</li>
+                    ))}
+                  </ul>
+                ) : null}
 
-                        
-                            <Link className="p-link" href={`/work/${project.slug}`}>
-                                    See Details &rarr;
-                                </Link>
-                        </div>
-                    </div>
-                ))}
+                <Link className="p-link" href={`/work/${project.slug}`}>
+                  See Details &rarr;
+                </Link>
+              </div>
             </div>
-
+          ))}
         </div>
-    </section>;
+      </div>
+    </section>
+  );
 }

a8a926f79725c0284fa4915bc04892656b8bb8c6
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-11-19 10:57:39 +0530

    improved project score and added sitemap and robot

--- app/components/Projects.jsx
+++ app/components/Projects.jsx
@@ -2,85 +2,18 @@
 import Image from "next/image";
 import Link from "next/link";
 import "./projects.css";
-import { useEffect, useState } from "react";
 import { projects } from "../data";
 export default function Projects() {
-  // const [projects, setProjects] = useState([]);
-  // const [loading, setLoading] = useState(true);
-
-  // useEffect(() => {
-  //   const fetchProjects = async () => {
-  //     try {
-  //       const res = await fetch(
-  //         "https://api.cosmicjs.com/v3/buckets/website-projects/objects?pretty=true&query=%7B%22type%22:%22projects%22%7D&limit=10&read_key=gV37hS5tuppUOgUwDAjiKKMYsOyBfVjD4HPoVg3iCKu9YFPv2c&depth=1&props=slug,title,metadata"
-  //       );
-  //       const data = await res.json();
-
-  //       if (data.objects) {
-  //         setProjects(data.objects);
-  //       }
-  //     } catch (error) {
-  //       console.error("Error fetching projects data:", error);
-  //     } finally {
-  //       setLoading(false);
-  //     }
-  //   };
-
-  //   fetchProjects();
-  // }, []);
-
-  // if (loading) {
-  //   return <p>Loading...</p>;
-  // }
   return (
     <div className="projects">
-      {/* {projects.map((project, index) => (
-        <div className="project" key={index}>
-          <Image
-            src={project.metadata.img_main.url}
-            alt={project.title}
-            width={344}
-            height={200}
-            priority={true}
-            style={{ width: "auto", height: "auto" }}
-          />
-          <div className="details">
-            <h3 className="p-title">{project.title}</h3>
-            <p className="description">
-              {project.metadata.description ? (
-                <span
-                  dangerouslySetInnerHTML={{
-                    __html: project.metadata.description,
-                  }}
-                />
-              ) : (
-                "No description available."
-              )}
-            </p>
-            {project.metadata.tech && project.metadata.tech.length > 0 && (
-              <ul className="tech-stack">
-                {project.metadata.tech.map((technology, techIndex) => (
-                  <li key={techIndex}>{technology}</li>
-                ))}
-              </ul>
-            )}
-
-            <Link className="p-link" href={project.metadata.href}>
-              See Live &rarr;
-            </Link>
-          </div>
-        </div>
-      ))} */}
-
       {projects.map((project, index) => (
         <div className="project" key={index}>
           <Image
             src={project.imgSrc}
             alt={project.title}
-            width={344}
+            width={400}
             height={200}
-            priority={true}
-            style={{ width: "auto", height: "auto" }}
+            priority="intrinsic"
           />
           <div className="details">
             <h2 className="p-title">{project.title}</h2>
@@ -93,7 +26,7 @@ export default function Projects() {
               </ul>
             )}
 
-            <Link className="p-link" href={project.href}>
+            <Link className="p-link" href={project.href} target="blank">
               See Live &rarr;
             </Link>
           </div>
--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -6,6 +6,11 @@ export default function Contact() {
       <section id="contact">
         <div className="container">
           <h1 className="contact-heading">Contact</h1>
+          <p>
+            Whether you are interested in hiring me, to work on a project
+            together or to discuss any other proposal, feel free to contact me
+            anytime. I am very much looking forward to your message!
+          </p>
           <ContactForm />
         </div>
       </section>

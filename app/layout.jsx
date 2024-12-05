ac2aafb53a1ed16e97835a26e90f77c31b46e94c
Dilip Kumar Maurya <curiousdilip@gmail.com>
2024-12-05 23:55:09 +0530

    Update layout.jsx

--- app/layout.jsx
+++ app/layout.jsx
@@ -4,6 +4,7 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import { Quicksand } from "next/font/google";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import Script from "next/script";
 
 // If loading a variable font, you don't need to specify the font weight
 const quicksand = Quicksand({
@@ -56,7 +57,47 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={quicksand.className}>
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-W78VYNSQ67"
+      />
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-W78VYNSQ67');
+          `,
+        }}
+      />
+
+      <Script
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){
+              w[l]=w[l]||[];
+              w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});
+              var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+              j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+              f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-TMR7S6CR');
+          `,
+        }}
+      />
       <body>
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-TMR7S6CR"
+            height="0"
+            width="0"
+            style="display:none;visibility:hidden"
+          ></iframe>
+        </noscript>
+
         <Header />
         {children}
         <Footer />

cbe84b23ed989ff5c7eb49cee7f20d6bf08319eb
Dilip Kumar Maurya <curiousdilip@gmail.com>
2025-01-03 06:09:13 +0000

    implemented framer on home page

--- app/layout.jsx
+++ app/layout.jsx
@@ -6,7 +6,6 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Script from "next/script";
 
-// If loading a variable font, you don't need to specify the font weight
 const quicksand = Quicksand({
   subsets: ["latin"],
   display: "swap",
--- app/page.jsx
+++ app/page.jsx
@@ -1,53 +1,14 @@
-"use client"
+"use client";
 import Link from "next/link";
 import "./page.css";
 import { motion } from "framer-motion";
-// export const metadata = {
-//   title: "Dilip Kumar | Frontend Web Developer",
-//   description:
-//     "Hi my name is Dilip Kumar Maurya and I am a Web developer based in New Delhi, India.",
-//   keywords: "curiousdilip, web developer dilip",
-//   authors: [
-//     {
-//       name: "Dilip Kumar",
-//       url: "https://www.dilipmaurya.in",
-//     },
-//   ],
-//   openGraph: {
-//     title: "Dilip Kumar | Frontend Web Developer",
-//     description:
-//       "Hi my name is Dilip Kumar Maurya and I am a Web developer based in New Delhi, India.",
-//     url: "https://www.dilipmaurya.in",
-//     siteName: "Dilip Kumar | Frontend Web Developer",
-//     images: [
-//       {
-//         url: "https://www.dilipmaurya.in/home-page.png",
-//         width: 800,
-//         height: 600,
-//         alt: "home page",
-//       },
-//     ],
-//     locale: "en_US",
-//     type: "website",
-//   },
-//   twitter: {
-//     card: "summary_large_image",
-//     site: "@curiousdilip",
-//     title: "Dilip Kumar | Frontend Web Developer",
-//     description:
-//       "Hi my name is Dilip Kumar Maurya and I am a Web developer based in New Delhi, India.",
-//     image: "https://www.dilipmaurya.in/home-page.png",
-//   },
-//   robots: "index, follow",
-// };
-
 export default function Home() {
   return (
     <>
       <section id="hero">
         <div className="container">
           <div className="info">
-            <motion.div
+            <motion.h1
               initial={{ opacity: 0, scale: 0.5 }}
               animate={{ opacity: 1, scale: 1 }}
               transition={{
@@ -55,36 +16,87 @@ export default function Home() {
                 ease: "anticipate",
               }}
             >
-              <h1>Dilip Kumar Maurya</h1>
-            </motion.div>
-            
-            <h2>Frontend Web Developer</h2>
-            <p>
+              Dilip Kumar Maurya
+            </motion.h1>
+            <motion.h2
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 1,
+                delay: 0.225,
+
+                ease: "anticipate",
+              }}
+            >
+              Frontend Web Developer
+            </motion.h2>
+            <motion.p
+              initial={{ opacity: 0, scale: 0.5 }}
+              animate={{ opacity: 1, scale: 1 }}
+              transition={{
+                duration: 1,
+                delay: 0.115,
+
+                ease: "anticipate",
+              }}
+            >
               Passionate about crafting stylish, modern websites, web services,
               and online stores, specializing in designing and developing
               services tailored to each client's unique needs.
-            </p>
+            </motion.p>
             <ul>
-              <li>
+              <motion.li
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1,
+                  ease: "anticipate",
+                }}
+              >
                 <Link href="/projects">
                   <i className="bi bi-laptop"></i> What I have Built?
                 </Link>
-              </li>{" "}
-              <li>
+              </motion.li>
+              <motion.li
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1,
+                  delay: 0.2,
+
+                  ease: "anticipate",
+                }}
+              >
                 <Link href="/about">
                   <i className="bi bi-person-vcard"></i> More about myself
                 </Link>
-              </li>{" "}
-              <li>
+              </motion.li>
+              <motion.li
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1,
+                  ease: "anticipate",
+                  delay: 0.3,
+                }}
+              >
                 <Link href="/resume">
                   <i className="bi bi-file-earmark-text"></i> My Latest Resume
                 </Link>
-              </li>{" "}
-              <li>
+              </motion.li>
+              <motion.li
+                initial={{ opacity: 0, scale: 0.5 }}
+                animate={{ opacity: 1, scale: 1 }}
+                transition={{
+                  duration: 1,
+                  ease: "anticipate",
+                  delay: 0.4,
+                }}
+              >
                 <Link href="/contact">
                   <i className="bi bi-telephone"></i> Contact Me
                 </Link>
-              </li>
+              </motion.li>
             </ul>
           </div>
         </div>
